fc6d799fc570855392effcd63a876c1f2387f257
casteli <matheuscasteli2@hotmail.com>
2022-04-26 17:36:56 -0300

    Adicionado codigo JS para navegar entre as paginas

---
+++ JS-paginas/js-main.js
@@ -0,0 +1,15 @@
+function buttonPlay() {
+  let escolha = "";
+  while (escolha != 1 && escolha != 2) {
+    escolha = prompt(
+      "Como declarar uma tag em HTML?\n 1: <nome da tag>  \n 2: {nome da tag}",
+    );
+    if (escolha === "1") {
+      window.location.href = "./fase2.html";
+    } else if (escolha === "2") {
+      window.location.href = "gameover";
+    } else {
+      alert("essa opção não existe");
+    }
+  }
+}
