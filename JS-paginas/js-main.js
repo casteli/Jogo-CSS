da349841affac7d24d1cae186a167aac5d258b1b
casteli <matheuscasteli2@hotmail.com>
2022-04-26 17:54:22 -0300

    nome de jogar alterado para pagina1 - começando a criar pagina2

--- JS-paginas/js-main.js
+++ JS-paginas/js-main.js
@@ -1,3 +1,4 @@
+// função do botão da pagina um
 function buttonPlay() {
   let escolha = "";
   while (escolha != 1 && escolha != 2) {
@@ -6,6 +7,23 @@ function buttonPlay() {
     );
     if (escolha === "1") {
       window.location.href = "./fase2.html";
+    } else if (escolha === "2") {
+      window.location.href = "gameover";
+    } else {
+      alert("essa opção não existe");
+    }
+  }
+}
+
+// função do botão da pagina doi
+function buttonPlay() {
+  let escolha = "";
+  while (escolha != 1 && escolha != 2) {
+    escolha = prompt(
+      "Como declarar uma tag em HTML?\n 1: <nome da tag>  \n 2: {nome da tag}",
+    );
+    if (escolha === "1") {
+      window.location.href = "./fase3.html";
     } else if (escolha === "2") {
       window.location.href = "gameover";
     } else {
