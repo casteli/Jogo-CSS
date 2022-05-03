6150b89ec11b313cdfaddbf8ea896d9e4f823b35
casteli <matheuscasteli2@hotmail.com>
2022-05-03 16:12:14 -0300

    JS Refeito do 0

--- JS-paginas/main.js
+++ JS-paginas/main.js
@@ -1,162 +1,34 @@
-//FASE BACKGROUND
-// função do botão da pagina um (backgorund)
-function buttonPlayB1() {
+const frases = [
+  "oque faz o elemento background-repeat: no-repeat? \n 1: Faz com que a imagem de fundo não se repita  \n 2: Proíbe que a imagem de fundo role com a página",
+  "oque faz o elemento background-size: cover;?\n 1: Faz com que a imagem de fundo cubra todo espaço no container  \n 2: faz a imagem de fundo cobrir somente o topo da pagina",
+  "Oque é background-image:?\n 1: faz parte do conjunto de propriedades utilizadas no CSS para definir o background  \n 2: Serve apenas para por fundo em uma <div>",
+  "Oque faz o font-size? \n 1: Serve para definir o tamanho da fonte  \n 2: muda a cor da fonte",
+  "Oque faz o font-family? \n 1: muda o estilo da fonte de acordo com a familia atribuida  \n 2: muda a largura da fonte",
+  "oque faz o font-weight:bold? \n 1: deixa a fonte em negrito \n 2: a fonte fica sublinhada",
+  "Para o que serve a propriedade margin-top? \n 1: Definir a margem superior de um elemento \n 2:  Definir a margem inferior de um elemento",
+  "Para o que serve a propriedade margin-left? \n 1: Definir a margem esquerda de um elemento \n Definir a margem direita de um elemento",
+  "Para o que serve a propriedade margin-right? \n 1: Definir a margem direita de um elemento  \n 2: Definir a margem de baixo de um elemento",
+];
+const gameOver = "../gameover/gameover.html";
+
+function buttonPlay(proximaFase, frase) {
   let escolha = "";
   while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "oque faz o elemento background-repeat: no-repeat;? \n 1: Faz com que a imagem de fundo não se repita  \n 2: Proíbe que a imagem de fundo role com a página",
-    );
+    escolha = prompt(selecionarFrase(frase));
     if (escolha === "1") {
-      window.location.href = "../fase-background/pagina2.html";
+      window.location.href = proximaFase;
     } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
+      window.location.href = gameOver;
     } else {
       alert("essa opção não existe");
     }
   }
 }
 
-// função do botão da pagina dois (backgorund)
-function buttonPlayB2() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "oque faz o elemento background-size: cover;?\n 1: Faz com que a imagem de fundo cubra todo espaço no container  \n 2: faz a imagem de fundo cobrir somente o topo da pagina",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-background/pagina3.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
+function selecionarFrase(selecionado) {
+  for (let i = 0; i < frases.length; i++) {
+    if (frases[i].search(selecionado) != -1) {
+      return frases[i];
     }
   }
-}
-
-// função do botão da pagina três (backgorund)
-function buttonPlayB3() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Oque é background-image:?\n 1: faz parte do conjunto de propriedades utilizadas no CSS para definir o background  \n 2: Serve apenas para por fundo em uma <div>",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-background/pagina4.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// FASE DAS FONTS A BAIXO
-
-// função do botão da pagina um (FONT)
-function buttonPlayF1() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Oque faz o font-size:? \n 1: Serve para definir o tamanho da fonte  \n 2: muda a cor da fonte",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-font/pagina2.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// função do botão da pagina dois (font)
-function buttonPlayF2() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Oque faz o font-family:? \n 1: muda o estilo da fonte de acordo com a familia atribuida  \n 2: muda a largura da fonte",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-font/pagina3.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// função do botão da pagina três (font)
-function buttonPlayF3() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "oque faz o font-weight:bold? \n 1: deixa a fonte em negrito \n 2: a fonte fica sublinhada",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-font/pagina4.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// --- FASE DA MARGIN A BAIXO
-
-// função do botão da pagina um (margin)
-function buttonPlayM1() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Para o que serve a propriedade margin-top? \n 1: Definir a margem superior de um elemento \n 2:  Definir a margem inferior de um elemento",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-margin/pagina2.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// função do botão da pagina dois (margin)
-function buttonPlayM2() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Para o que serve a propriedade margin-left? \n 1: Definir a margem esquerda de um elemento \n Definir a margem direita de um elemento",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-margin/pagina3.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// função do botão da pagina três (margin)
-function buttonPlayM3() {
-  let escolha = "";
-  while (escolha != 1 && escolha != 2) {
-    escolha = prompt(
-      "Para o que serve a propriedade margin-right? \n 1: Definir a margem direita de um elemento  \n 2: Definir a margem de baixo de um elemento ",
-    );
-    if (escolha === "1") {
-      window.location.href = "../fase-margin/pagina4.html";
-    } else if (escolha === "2") {
-      window.location.href = "../gameover/gameover.html";
-    } else {
-      alert("essa opção não existe");
-    }
-  }
-}
-
-// função do botao inicio
-function buttonInicio() {
-  window.location.href = "../index.html";
 }
